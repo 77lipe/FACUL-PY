b12e8c8f1700417b38ef12bfdf64e59850ac6186
FELIPE VIEIRA DOS SANTOS <felipe.santos438@senaisp.edu.br>
2026-02-11 22:45:59 -0300

    tentando resolver exer.12

--- exercicio12.py
+++ exercicio12.py
@@ -1,10 +1,16 @@
 number1 = int(input("Digite um número:\n"))
 
 for i in range(2,number1):
-    if number1 % i != 0:
-        primo = number1
-    elif number % i:
+    primo = 0
+    cont = number1 % i
+    #print(f'Cont = {cont}, O valor de I: {i}')
+    if cont == 0:
+        primo = cont
+        if primo == 0:
+            print(f'O numero {number1} NÃO é um primo')
+        else:
+            print(f'{number1} é um número PRIMO')
         
-print(f'O número {number1} é Primo')
-
+        break   
+        
 
