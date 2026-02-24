0ef1185baa27aa96be9ac3188650d181d5a140b7
FELIPE VIEIRA DOS SANTOS <felipe.santos438@senaisp.edu.br>
2026-02-24 20:09:25 -0300

    feat: exer Físico

---
+++ aula/exerFisico_PAR.py
@@ -0,0 +1,6 @@
+l = [1,2,3,4,5,6,7,8,9,10]
+
+for i in l:
+    r = i % 2
+    if r == 0:
+        print(f'O número {i} é par')
---
+++ aula/forDuplo.py
@@ -0,0 +1,5 @@
+palavra = 'sorvete'
+
+for letras in palavra:
+    if letras == "v":
+        print("Letra V identificada")
